4574c0bff31bb93cbb25228e75556b4a858956d4
Zhe Chen <zhechen@MacBook-Pro.local>
2022-01-05 22:30:11 +0800

    Update

--- tests/Solution_2089.java
+++ tests/Solution_2089.java
@@ -11,51 +11,41 @@ import org.junit.jupiter.api.Test;
 
 class Solution_2089 {
     public List<Integer> targetIndices(int[] nums, int target) {
-        if(nums == null || nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return null;
         }
-        
+
         int lessThanTarget = 0;
         int eqToTarget = 0;
-        for(int num : nums) {
-            if(num == target) {
-                eqToTarget+=1;
-            } else if(num < target) {
-                lessThanTarget+=1;
+        for (int num : nums) {
+            if (num == target) {
+                eqToTarget += 1;
+            } else if (num < target) {
+                lessThanTarget += 1;
             }
         }
-        
+
         List<Integer> result = new ArrayList<Integer>();
-        for(int i = 0; i < eqToTarget; i++) {
+        for (int i = 0; i < eqToTarget; i++) {
             result.add(lessThanTarget++);
         }
-        
+
         return result;
     }
 
     @Test
     void test() {
         Assertions.assertEquals(
-                targetIndices(new int[] { 1, 2, 5, 2, 3 }, 2), new ArrayList<Integer>() {
-                    {
-                        add(1);
-                        add(2);
-                    }
-                });
+                targetIndices(TestHelper.arrayStringToIntArray("[1,2,5,2,3]"), 2),
+                TestHelper.arrayStringToIntList("[1,2]"));
 
         Assertions.assertEquals(
-                targetIndices(new int[] { 1, 2, 5, 2, 3 }, 3), new ArrayList<Integer>() {
-                    {
-                        add(3);
-                    }
-                });
+                targetIndices(TestHelper.arrayStringToIntArray("[1,2,5,2,3]"), 3),
+                TestHelper.arrayStringToIntList("[3]"));
 
         Assertions.assertEquals(
-                targetIndices(new int[] { 1, 2, 5, 2, 3 }, 5), new ArrayList<Integer>() {
-                    {
-                        add(4);
-                    }
-                });
+                targetIndices(TestHelper.arrayStringToIntArray("[1,2,5,2,3]"), 5),
+                TestHelper.arrayStringToIntList("[4]"));
 
     }
 }
--- tests/TestHelper.java
+++ tests/TestHelper.java
@@ -1,9 +1,31 @@
 package tests;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class TestHelper {
     public static <E> boolean compareList(List<E> result, List<E> expected) {
         return result.equals(expected);
+    }
+
+    public static int[] arrayStringToIntArray(String stringArray) {
+        int[] intArray = Arrays.stream(stringArray.substring(1, stringArray.length() - 1).split(","))
+                .map(String::trim).mapToInt(Integer::parseInt).toArray();
+        return intArray;
+    }
+
+    public static int[][] arrayStringTo2DIntArray(String stringArray) {
+        return Arrays.stream(stringArray.substring(2, stringArray.length() - 2).split("\\],\\["))
+                .map(e -> Arrays.stream(e.split("\\s*,\\s*")).map(String::trim).map(Integer::parseInt).mapToInt(i -> i)
+                        .toArray())
+                .toArray(int[][]::new);
+    }
+
+    public static List<Integer> arrayStringToIntList(String stringArray) {
+        List<Integer> list = Arrays.stream(arrayStringToIntArray(stringArray))
+                .boxed()
+                .collect(Collectors.toList());
+        return list;
     }
 }
