7c1735c05baaa85b0d543d79ea18c41157e2c7c1
mephisto <mephistophilis@gmail.com>
2022-01-25 23:44:33 +0800

    update

---
+++ tests/Solution_201.java
@@ -0,0 +1,34 @@
+package tests;
+//201. Bitwise AND of Numbers Range
+
+//https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Solution_201 {
+
+  public int rangeBitwiseAnd(int left, int right) {
+    if(left == 0){
+      return 0;
+  }
+  int moveFactor = 1;
+  while(left != right){
+      left >>= 1;
+      right >>= 1;
+      moveFactor <<= 1;
+  }
+  return left * moveFactor;
+  }
+
+  //0000000000000101 
+  //0000000000000110
+  //0000000000000111
+
+
+  @Test
+  void test() {
+    Assertions.assertEquals(4,
+        rangeBitwiseAnd(5, 7));
+  }
+}
